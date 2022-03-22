d91f4ccb2313f0f7a488061426110e4c154d8fd0
imj22 <98693440+imj22@users.noreply.github.com>
2022-03-22 15:41:24 +0100

    commit tree 62c626e1378ebde362cabbe98fe03972984bf727

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    const changeText = document.querySelector("#text")
+    // console.log(changeText);
+    changeText.textContent = "This is really cool!";
+});
+
+
+// document.addEventListener("DOMContentLoaded", textChange){
+//     console.log("The DOM has loaded");
+//   };
+
+//   function textChange() {
+//     const changeText = document.querySelector("text")
+//     changeText.textContent = "This is really cool!";
+//     console.log(changeText);
+//   }
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
